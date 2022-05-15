f1c29aa6bc80c790863e262bc74ab0b1b14b2ece
Tiago Ameller <tiago.ameller@gmail.com>
2022-05-15 18:58:50 +0200

    add devise; user and brand models

--- Gemfile
+++ Gemfile
@@ -38,6 +38,9 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 gem 'rubocop'
 gem 'rubocop-rails'
+gem 'rubocop-rspec'
+
+gem 'devise', '~> 4.8', '>= 4.8.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ db/migrate/20220515163453_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email, null: false, default: ''
+      t.integer :admin_level
+
+      t.timestamps
+    end
+
+    add_index :users, :email, unique: true
+  end
+end
