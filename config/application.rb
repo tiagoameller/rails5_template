3e95ec102fbf7efc836828a5ee92c92213da36db
Tiago Ameller <tiago.ameller@gmail.com>
2022-05-31 19:02:59 +0200

    devise controllers and actions

--- config/application.rb
+++ config/application.rb
@@ -16,9 +16,40 @@ module R5template
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    POUNOU_DOMAIN = 'pounoupro.cat'.freeze
     # show current exercise from system environment
     config.year = (ENV.fetch('YEAR') { Time.zone.today.year }).to_i
     config.year_last_digit = config.year.to_s[-1]
+
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.default_url_options =
+      if Rails.env.production?
+        { host: "http://#{config.year}.#{POUNOU_DOMAIN}" }
+      else
+        { host: 'localhost:3000' }
+      end
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings =
+      if Rails.env.production?
+        {
+          domain: POUNOU_DOMAIN,
+          user_name: ENV.fetch('MAIL_USER', nil),
+          password: ENV.fetch('MAIL_PASS', nil),
+          authentication: 'plain',
+          port: 25,
+          address: 'smtp.eu.mailgun.org'
+        }
+      else
+        {
+          domain: POUNOU_DOMAIN,
+          user_name: ENV.fetch('PN_MAIL_USER', nil),
+          password: ENV.fetch('PN_MAIL_PASS', nil),
+          authentication: 'plain',
+          port: 25,
+          address: 'smtp.eu.mailgun.org'
+        }
+      end
 
     # http://www.createdbypete.com/articles/working-with-locales-and-time-zones-in-rails/
     config.time_zone = 'Madrid'
